414c06f1053fd536b2321de193e54a4f7a3b255d
FurWaz <fur.waz06@gmail.com>
2024-09-02 09:02:38 +0200

    Added base website files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex w-full h-full ">
+        <RouterView />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        RouterView
+    }
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold">FurWaz - Store</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'HomeView'
+});
+</script>
